813a0d2e5344c4de322ec6e122736aa293ea619c
meylaniic <meylaniic@gmail.com>
2025-05-25 11:52:16 +0700

    praktikum 2

--- SLLMain10.java
+++ SLLMain10.java
@@ -1,12 +1,32 @@
-
-import java.util.Scanner;
-
 public class SLLMain10 {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+        //Scanner sc = new Scanner(System.in);
         SingleLinkedList10 sll = new SingleLinkedList10();
+        Mahasiswa10 mhs1 = new Mahasiswa10("24212200", "Alvaro", "1A", 4.0);
+        Mahasiswa10 mhs2 = new Mahasiswa10("23212201", "Bimon", "2B", 3.8);
+        Mahasiswa10 mhs3 = new Mahasiswa10("22212202", "Cintia ", "3C", 3.5);
+        Mahasiswa10 mhs4 = new Mahasiswa10("21212203", "Dirga", "4D", 3.6);
 
-        System.out.print("Masukkan jumlah data yang ingin dimasukkan : ");
+        sll.addLast(mhs4);
+        sll.addLast(mhs3);
+        sll.addLast(mhs2);
+        sll.addLast(mhs1);
+
+        System.out.println("Data index 1: ");
+        sll.getData(1);
+        System.out.println("Data mahasiswa Bimon berada pada index : " + sll.indexOf("bimon"));
+        System.out.println();
+
+        
+
+        sll.removeFirst();
+        sll.removeLast();
+        sll.print();
+        sll.removeAt(0);
+        sll.print();
+
+
+        /*System.out.print("Masukkan jumlah data yang ingin dimasukkan : ");
         int jumlah = sc.nextInt();
         sc.nextLine();
         for (int i = 0; i < jumlah; i++) {
@@ -26,6 +46,6 @@ public class SLLMain10 {
             System.out.println();
         }
         
-        sll.print();
+        sll.print(); */
     }
 }
